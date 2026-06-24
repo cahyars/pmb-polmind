c4ad6a908c1faa16ef15d7e3fde4b948046ef546
Cahya Ramdan Syah <cahyaramdansyah12@gmail.com>
2026-06-24 09:35:17 +0700

    Setup base Blade layouts and view structure

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('public.home');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/camaba/dashboard', function () {
+    return view('camaba.dashboard');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
 });
